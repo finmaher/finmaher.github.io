8296fa04259119254a97c4b190f3c7a9790e062f
miyazakid <mfintan@gmail.com>
2019-03-23 23:38:19 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-var copyCodeBtn = document.querySelector('#js-emailcopybtn');
-var emailLink = document.querySelector('#code-to-copy');
-var initContent = emailLink.textContent;
+let copyCodeBtn = document.querySelector('#js-emailcopybtn');
+let emailLink = document.querySelector('#code-to-copy');
+const initContent = emailLink.textContent;
 
 copyCodeBtn.addEventListener('click', function(event)
 {
     // Select the email link anchor text
- emailLink.insertAdjacentText('beforeend','@me.com');
-    emailLink = document.querySelector('#code-to-copy');
-  var range = document.createRange();
+  emailLink.insertAdjacentText('beforeend','@me.com');
+  emailLink = document.querySelector('#code-to-copy');
+  let range = document.createRange();
     range.selectNode(emailLink);
     window.getSelection().addRange(range);
 
     try
     {
         // Now that we've selected the anchor text, execute the copy command
-        var successful = document.execCommand('copy');
-        var msg = successful ? 'successful' : 'unsuccessful';
+        let successful = document.execCommand('copy');
+        let msg = successful ? 'successful' : 'unsuccessful';
         console.log('Copy email command was ' + msg);
     }
     catch (err)
@@ -27,5 +27,4 @@ copyCodeBtn.addEventListener('click', function(event)
     // removeRange(range) when it is supported
     window.getSelection().removeAllRanges();
     emailLink.textContent = initContent;
-    textInput.value = '';
 });
