9e266415dab114e139eb6034e3ff59e8906189bd
Fintan Maher <34825464+miyazakid@users.noreply.github.com>
2019-11-01 03:13:22 -0400

    Add build to Github pages

---
+++ precache-manifest.1d24c8d0fdbff420d4b28a419977e4a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3d2e5d11aefb18f444106be475871b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d44782fcfb4092946fd",
+    "url": "/static/css/main.1c39f643.chunk.css"
+  },
+  {
+    "revision": "90644b3fd3bd4e3a9c61",
+    "url": "/static/js/2.057877b5.chunk.js"
+  },
+  {
+    "revision": "2d44782fcfb4092946fd",
+    "url": "/static/js/main.a03e26be.chunk.js"
+  },
+  {
+    "revision": "911dbea3282e21f970c7",
+    "url": "/static/js/runtime-main.2c300789.js"
+  }
+]);
